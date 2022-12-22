c3622757e48429673a6350cf51e0514d067b9173
khaled <khaled1422_8@gmail.com>
2022-12-22 16:22:59 +0300

    profile

--- lib/Component/campComponent/CardCamp.dart
+++ lib/Component/campComponent/CardCamp.dart
@@ -10,84 +10,89 @@ class CardCamp extends StatelessWidget {
       required this.NameOfCamp,
       required this.DateOfCamp,
       required this.detailsOfCamp,
-      required this.NamberOfStudent, required this.Img});
+      required this.NamberOfStudent, required this.Img, this.onTap});
   final String NameOfCamp;
   final String DateOfCamp;
   final String? detailsOfCamp;
   final String NamberOfStudent;
   final String Img;
+  final Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsetsDirectional.all(10),
-      padding: EdgeInsetsDirectional.all(20),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadiusDirectional.circular(10),
-        color: HexColor("#dbd9de"),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Row(
-                children: [
-                  CircleAvatar(
-                      backgroundColor: Colors.white,
-                     radius: 30,
-                      child:ClipOval(child: Image.asset(Img,width: 90, fit: BoxFit.cover,),),),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    NameOfCamp,
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 17,
-                        fontWeight: FontWeight.w600),
-                  ),
-                ],
-              ),
-              Spacer(),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Icon(Icons.date_range),
-              ),
-              Text(
-                DateOfCamp,
-                style: TextStyle(
-                  color: Color.fromARGB(255, 135, 134, 134),
-                  fontSize: 16,
+    return InkWell(
+      onTap:onTap ,
+      
+      child: Container(
+        margin: EdgeInsetsDirectional.all(10),
+        padding: EdgeInsetsDirectional.all(20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadiusDirectional.circular(10),
+          color: HexColor("#dbd9de"),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                        backgroundColor: Colors.white,
+                       radius: 30,
+                        child:ClipOval(child: Image.asset(Img,width: 90, fit: BoxFit.cover,),),),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      NameOfCamp,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ],
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          AutoSizeText(
-            detailsOfCamp!,
-            style: TextStyle(fontSize: 16),
-            maxLines: 5,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              ImageIcon(
-                AssetImage("images/Vector.png"),
-              ),
-              const SizedBox(
-                width: 7,
-              ),
-              Text(
-                NamberOfStudent,
-                style: TextStyle(color: HexColor("#117c78"), fontSize: 17),
-              ),
-            ],
-          ),
-        ],
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(Icons.date_range),
+                ),
+                Text(
+                  DateOfCamp,
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 135, 134, 134),
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            AutoSizeText(
+              detailsOfCamp!,
+              style: TextStyle(fontSize: 16),
+              maxLines: 5,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ImageIcon(
+                  AssetImage("images/Vector.png"),
+                ),
+                const SizedBox(
+                  width: 7,
+                ),
+                Text(
+                  NamberOfStudent,
+                  style: TextStyle(color: HexColor("#117c78"), fontSize: 17),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/View/profile/profileView.dart
@@ -0,0 +1,87 @@
+import 'package:final_project/Component/Profile/ItemOfProfile.dart';
+import 'package:final_project/View/Camp/CampPresnt.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class profile extends StatelessWidget {
+  const profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff4A3A75),
+      body: Column(
+        children: [
+          Container(
+            height: Get.height / 6,
+            color: Color(0xff4A3A75),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 40, top: 30, right: 50),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "خالد",
+                    style: TextStyle(fontSize: 26, color: Color(0xffFFFFFF)),
+                  ),
+                  ImageIcon(AssetImage("images/mdi_location-exit.png"),
+                      size: 50, color: Color(0xffFFFFFF)),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+              child: Container(
+            height: Get.height / 1,
+            width: Get.width,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Color(0xffFFFFFF)),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 170,
+                  child: CircleAvatar(
+                    backgroundColor: Color(0xff4A3A75),
+                    radius: 70,
+                    child: ClipOval(
+                      child: Image.asset(
+                        "images/images 1.png",
+                        width: 130,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 366,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0xffCBCBCB)),
+                  child: Column(children: [
+                    ItemProfile(
+                      name: "بيانات شخصيه",
+                      ontap: () {
+                        
+                        //Get.to();
+                      },
+                    ),
+                    ItemProfile(name: "منشوراتي"),
+                    ItemProfile(
+                      name: "تحديث كلمة السر",
+                    ),
+                    ItemProfile(name: "سياسة الاستخدام"),
+                    ItemProfile(name: "الدعم الفني"),
+                  ]),
+                )
+              ],
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+}
