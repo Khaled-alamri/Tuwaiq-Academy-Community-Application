46932aa1bfe29bfd379ff2899fac72ed57250ebe
Mansour <mnasor3000@outlook.sa>
2022-12-22 21:08:08 +0300

    connect project to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:final_project/Localization/Languages.dart';
 import 'package:final_project/Router/Routers.dart';
 import 'package:final_project/View/OnBoarding/PageView.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
