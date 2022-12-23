44435291ceac74e7dae1713bbce2074b0ec62a35
khaled <khaled1422_8@gmail.com>
2022-12-23 20:52:12 +0300

    don my post

--- lib/View/profile/My_Post.dart
+++ lib/View/profile/My_Post.dart
@@ -1,4 +1,5 @@
 import 'package:final_project/Component/TitelCamp.dart';
+import 'package:final_project/Component/custom%20article%20card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -15,9 +16,10 @@ class My_Post extends StatelessWidget {
      appBar: PreferredSize(
           preferredSize: Size.fromHeight(180), // Set this height
           child: SafeArea(
-            child: Column(
+            child: ListView(
               children: [
               CustomTitleWithRadius(Title:  "منشوراتي",height1: 150,),
+              
                     ],
                   ),
           ),
@@ -25,7 +27,27 @@ class My_Post extends StatelessWidget {
 
       body: ListView(
         children: [
-                
+                MyCustomArticleCard(
+                  postTitle: "Test Zhar",
+                  likeCont: 15,
+                  PostBody: "Hi this is the ",
+                  PostOwnrName: "Zhara",
+                  cardDate: "12-17-2022",
+                  commentCont: 4),
+                  MyCustomArticleCard(
+                  postTitle: "Test Zhar",
+                  likeCont: 2,
+                  PostBody: "Hi  ",
+                  PostOwnrName: "Zhara",
+                  cardDate: "12-17-2022",
+                  commentCont: 1),
+                  MyCustomArticleCard(
+                  postTitle: "Test Zhar",
+                  likeCont: 2,
+                  PostBody: "Hi  ",
+                  PostOwnrName: "Zhara",
+                  cardDate: "12-17-2022",
+                  commentCont: 1),
         ],
       ),
     );
