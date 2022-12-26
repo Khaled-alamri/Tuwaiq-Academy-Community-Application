b84ce80b0fb40f445cb3cffccfae6153daa50d8d
Sarah <sarah.algodah@outlook.com>
2022-12-26 20:36:13 +0300

    Query Task

---
+++ lib/SerachController.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
+
+class SearchController extends GetxController {
+  List searchResult = [];
+
+  final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  searchFromeFirbase(String query) async {
+    var result = await FirebaseFirestore.instance
+        .collection("Test")
+        .where(
+          "title",
+          isEqualTo: query,
+        )
+        .get();
+
+    searchResult = result.docs.map((e) => e.data()).toList();
+    update();
+  }
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:final_project/Localization/Languages.dart';
-import 'package:final_project/Router/Routers.dart';
-import 'package:final_project/View/OnBoarding/PageView.dart';
-import 'package:final_project/View/Splash_Screen/SplashScreen.dart';
+import 'package:final_project/Search.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -16,9 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home: SplashScreen(),
-      getPages: getPages,
-      translations: Languages(),
+      home: SearchPage(),
       locale: const Locale('ar'), // The Languages shulde be Arabic when tha App Start
     );
   }
