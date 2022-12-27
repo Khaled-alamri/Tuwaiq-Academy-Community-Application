277c493a08d1ab6ccf2a972cf533dd4a8c7a7843
khaled <khaled1422_8@gmail.com>
2022-12-27 17:41:35 +0300

    mm

---
+++ lib/Controller/GetImage.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:final_project/Controller/Article%20Controller.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:get/get.dart';
+
+class GetImage{
+  ArticleController ArticleControllerImage = Get.put(ArticleController());
+  var instance = FirebaseStorage.instance;
+  AddImages({required File Image}) async {
+    try {
+      final file = Image;
+      final ref =
+      await instance.ref().child("images/${ArticleControllerImage.postUID}.png");
+      await ref.putFile(file);
+      return ArticleControllerImage.image = file.toString();
+    } catch (e) {
+      print(e);
+    }
+  }
+}
+
+getImages({required String postUID}) async {
+  try {
+    var ref = await FirebaseStorage.instance.ref("images").list();
+    ref.items.forEach((element) {
+      if (element.name == postUID + ".png") {
+        print(element.name);
+      }
+    });
+  } catch (e) {
+    print(e);
+  }
+}
