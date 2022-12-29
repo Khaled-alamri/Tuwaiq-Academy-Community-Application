4b52b3c5f07ac8dccd13a194a18384bd85935fad
Sarah <sarah.algodah@outlook.com>
2022-12-29 19:38:46 +0300

    SignUpAndSignIn Add Guest Button

--- lib/View/Sing/signup_and_signin_screen.dart
+++ lib/View/Sing/signup_and_signin_screen.dart
@@ -1,5 +1,7 @@
 import 'package:final_project/Component/CustomButton.dart';
 import 'package:final_project/Component/SingComponenr/logoWithTitle.dart';
+import 'package:final_project/Custom/CustomSlideButton.dart';
+import 'package:final_project/Packages/package.dart';
 import 'package:final_project/Router/Routers.dart';
 import 'package:final_project/View/Sing/signin_screen.dart';
 import 'package:final_project/View/Sing/signup_screen.dart';
@@ -8,6 +10,8 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 import 'package:hexcolor/hexcolor.dart';
+
+import '../Guste/GuestAppLayout.dart';
 
 class SignUpAndSignIn extends StatelessWidget {
   const SignUpAndSignIn({Key? key}) : super(key: key);
@@ -51,54 +55,41 @@ class SignUpAndSignIn extends StatelessWidget {
                   const SizedBox(
                     height: 50,
                   ),
-                  Text(
-                    "-------------------- أو --------------------",
-                    style: TextStyle(color: Colors.black, fontSize: 22),
+                         SizedBox(
+                    width: 200,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: 1,
+                          width: 60,
+                          color: primaryColor.withOpacity(0.6),
+                        ),
+                        Text(
+                          "أو",
+                          style: TextStyle(
+                              color: primaryColor.withOpacity(0.6),
+                              fontSize: 22),
+                        ),
+                        Container(
+                          height: 1,
+                          width: 60,
+                          color: primaryColor.withOpacity(0.6),
+                        ),
+                      ],
+                    ),
                   ),
                   const SizedBox(
                     height: 40,
                   ),
-                  Container(
-                    padding: EdgeInsetsDirectional.only(
-                      start: 10,
-                      end: 15,
-                      top: 0,
-                      bottom: 0,
-                    ),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5.0),
-                      color: HexColor("#078378"),
-                    ),
-                    child: Row(
-                      children: [
-                        Container(
-                          padding: EdgeInsetsDirectional.all(5),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(5.0),
-                            color: Colors.white,
-                          ),
-                          child: Icon(
-                            Icons.person_add,
-                            color: HexColor("#058378"),
-                          ),
-                        ),
-                        Expanded(
-                          child: Center(
-                            child: MaterialButton(
-                              onPressed: () {},
-                              child: Text(
-                                "مرحبًا بك كضيف",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20.0,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+                    CustomSlideButton(
+                    slideIcon: "images/guste.png",
+                    text: "مرحباً بك كضيف",
+                    onSubmit: () {
+                      Get.off(GuestAppLayout());
+                    },
                   )
+                  
                 ],
               ),
             ),
