6cf1bb09afafc28b341d53898a4d6159027cb52c
Sarah <sarah.algodah@outlook.com>
2022-12-29 22:31:40 +0300

    Edit on Add Camp + Add Admin Folder

---
+++ lib/View/Admin/AdminAppLayout.dart
@@ -0,0 +1,92 @@
+import 'package:final_project/Controller/Profile%20controller.dart';
+import 'package:final_project/Packages/package.dart';
+import 'package:final_project/View/Admin/AdminHomeScreen.dart';
+import 'package:final_project/View/Admin/AdminTitelPage.dart';
+import 'package:final_project/View/Camp/CampPresnt.dart';
+import 'package:final_project/View/Home/home_screen.dart';
+import 'package:final_project/View/article/search_field.dart';
+import 'package:final_project/View/profile/profileView.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class AdminAppLayout extends StatefulWidget {
+  @override
+  State<AdminAppLayout> createState() => _AdminAppLayoutState();
+}
+
+class _AdminAppLayoutState extends State<AdminAppLayout> {
+  int currentIndex = 0;
+  profileController C_Profile = Get.put(profileController());
+  List<Widget> bottomScreens = [
+    AdminHomeScreen(),
+    SearchField(),
+    AdminTitelPage(),
+    profile(),
+  ];
+
+  void changeBottomNav(int index) {
+    currentIndex = index;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: Get.height,
+      width: Get.width,
+      decoration: const BoxDecoration(
+        gradient:
+            LinearGradient(colors: [Color(0xFFD9FFF3), Color(0xFFFFE1F9)]),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: bottomScreens[currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.transparent,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          selectedItemColor: HexColor("#433b81"),
+          type: BottomNavigationBarType.fixed,
+          elevation: 0,
+          unselectedItemColor: primaryColor.withOpacity(0.4),
+          onTap: (index) {
+            setState(() {
+              changeBottomNav(index);
+            });
+          },
+          currentIndex: currentIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                size: 30,
+              ),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search_sharp,
+                size: 30,
+              ),
+              label: "Search",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.house,
+                size: 30,
+              ),
+              label: "University",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                size: 30,
+              ),
+              label: "Profile",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
